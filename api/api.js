33c685e5017c397f9e5d3c21d7482905d951da61
LihuaXiEvan <784850710@qq.com>
2024-10-01 19:38:51 +0800

    api

---
+++ api/api.js
@@ -0,0 +1,66 @@
+
+var app = require("express").Router();
+const DBPool = require('./crowdfunding_db.js')
+
+app.get('/getFundraisersList', (req, res) => {
+    let searchSql = 'SELECT * from `FUNDRAISER`'
+    DBPool.query(searchSql, [], (results) => {
+        let result = results.results
+        res.send(result);
+    })
+});
+app.get("/getActiveFundraisers", (req, res) => {
+    let param = req.query
+    let searchSql = 'SELECT * from `FUNDRAISER` WHERE ACTIVE = ?'
+    DBPool.query(searchSql, [param.active], (results) => {
+        let result = results.results
+        res.send(result);
+    })
+});
+app.get("/getOneFundraiser", (req, res) => {
+    let param = req.query
+    if(!param.CATEGORYID && !param.FUNDRAISERID){
+        let searchSql = 'SELECT * from `FUNDRAISER`'
+        DBPool.query(searchSql, [], (results) => {
+            let result = results.results
+            res.send(result);
+        })
+    } else if(param.CATEGORYID && !param.FUNDRAISERID){
+        let searchSql = 'SELECT * from `FUNDRAISER` WHERE CATEGORYID = ?'
+        DBPool.query(searchSql, [param.CATEGORYID], (results) => {
+            let result = results.results
+            res.send(result);
+        })
+    } else if(!param.CATEGORYID && param.FUNDRAISERID){
+        let searchSql = 'SELECT * from `FUNDRAISER` WHERE FUNDRAISERID = ?'
+        DBPool.query(searchSql, [param.FUNDRAISERID], (results) => {
+            let result = results.results
+            res.send(result);
+        })
+    } else {
+        let searchSql = 'SELECT * from `FUNDRAISER` WHERE FUNDRAISERID = ? AND CATEGORYID = ?'
+        DBPool.query(searchSql, [param.FUNDRAISERID,param.CATEGORYID], (results) => {
+            let result = results.results
+            res.send(result);
+        })
+    }
+    
+});
+app.get("/Details", (req, res) => {
+    let param = req.query
+    let searchSql = 'SELECT * from `FUNDRAISER` WHERE FUNDRAISERID = ?'
+    DBPool.query(searchSql, [param.FUNDRAISERID], (results) => {
+        let result = results.results
+        res.send(result);
+    })
+});
+app.get("/List", (req, res) => {
+    let searchSql = 'SELECT * from `CATEGORY`'
+    DBPool.query(searchSql, [], (results) => {
+        let result = results.results
+        res.send(result);
+    })
+});
+
+
+module.exports = app;
