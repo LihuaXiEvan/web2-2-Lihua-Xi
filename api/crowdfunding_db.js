84a6f0007de820902f13efd32eeca64859d42191
LihuaXiEvan <784850710@qq.com>
2024-10-02 18:00:35 +0800

    zhushi

--- api/crowdfunding_db.js
+++ api/crowdfunding_db.js
@@ -1,5 +1,7 @@
+//Introducing MySQL module for database operations
 class DBPool {
 	constructor() {
+		//Create database connection configuration
 		this.mysql = require("mysql");
 		this.config = {
 			host: 'localhost', 
@@ -8,17 +10,22 @@ class DBPool {
 			password: 'admin', 
 			database: 'crowdFunding_db',
 		};
+		//Create a connection pool to improve database connection efficiency
 		this.pool = this.mysql.createPool(this.config);
 	}
+
+	//Execute SQL query
 	query(sql, params, callBack) {
+		//Retrieve a connection from the connection pool
 		this.pool.getConnection((err, connection) => {
 			if (err) {
-				throw err;
+				throw err;//Handling connection errors
 			}
+			//Execute query
 			connection.query(sql, params, (err, results, fields) => {
 				connection.release();
 				if (err) {
-					throw err;
+					throw err;//Handling query errors
 				}
 				callBack && callBack({ results, fields });
 			});
@@ -26,4 +33,5 @@ class DBPool {
 	}
 }
 
+//Export DBPool instance
 module.exports = new DBPool();
